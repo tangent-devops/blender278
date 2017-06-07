ebe819c68e3303805225dd3657ab5f1b848ef056
Stefan Werner <stewreo@gmail.com>
2017-06-07 15:05:35 +0200

    Cycles: Embree support for ribbon line segments and subsurface scattering

--- intern/cycles/device/device_cpu.cpp
+++ intern/cycles/device/device_cpu.cpp
@@ -618,6 +618,9 @@ public:
 		int start_sample = tile.start_sample;
 		int end_sample = tile.start_sample + tile.num_samples;
 
+		_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+		_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+		
 		for(int sample = start_sample; sample < end_sample; sample++) {
 			if(task.get_cancel() || task_pool.canceled()) {
 				if(task.need_finish_queue == false)
